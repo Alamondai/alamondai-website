2f71833d5864ffab955bcefdd10169dc4847a6a6
natnaelengeda <nattynengeda@gmail.com>
2024-04-10 22:43:18 +0300

    Update axios base URL to use HTTPS in utils/axios.ts

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://api.alamondai.com",
+  baseURL: "https://api.alamondai.com",
   timeout: 30000,
   withCredentials: true,
   headers: {
